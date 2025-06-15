7e8f6a984434b6539380898f3cc84c055cda6a3a
affaqn1 <affaqahmed8411@gmail.com>
2025-06-15 13:21:21 +0530

    Admin View Payments

--- ETS/AdminViewPaymentsForm.Designer.cs
+++ ETS/AdminViewPaymentsForm.Designer.cs
@@ -2,38 +2,51 @@
 {
     partial class AdminViewPaymentsForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridViewPayments;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewPayments = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPayments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPayments
+            // 
+            this.dataGridViewPayments.AllowUserToAddRows = false;
+            this.dataGridViewPayments.AllowUserToDeleteRows = false;
+            this.dataGridViewPayments.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewPayments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPayments.Location = new System.Drawing.Point(20, 20);
+            this.dataGridViewPayments.Name = "dataGridViewPayments";
+            this.dataGridViewPayments.ReadOnly = true;
+            this.dataGridViewPayments.RowHeadersVisible = false;
+            this.dataGridViewPayments.RowTemplate.Height = 25;
+            this.dataGridViewPayments.Size = new System.Drawing.Size(740, 400);
+            this.dataGridViewPayments.TabIndex = 0;
+            // 
+            // AdminViewPaymentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminViewPaymentsForm";
+            this.ClientSize = new System.Drawing.Size(780, 450);
+            this.Controls.Add(this.dataGridViewPayments);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AdminViewPaymentsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "All Payments";
+            this.Load += new System.EventHandler(this.AdminViewPaymentsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPayments)).EndInit();
+            this.ResumeLayout(false);
         }
-
-        #endregion
     }
 }
--- ETS/AdminViewPaymentsForm.cs
+++ ETS/AdminViewPaymentsForm.cs
@@ -6,11 +6,25 @@ namespace ETS
     public partial class AdminViewPaymentsForm : Form
     {
         private User loggedInUser;
+        private AdminController adminController;
 
         public AdminViewPaymentsForm(User user)
         {
             InitializeComponent();
             loggedInUser = user;
+            adminController = new AdminController();
+        }
+
+        private void AdminViewPaymentsForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridViewPayments.DataSource = adminController.GetAllPayments();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load payments: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
