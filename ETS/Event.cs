bdecff8327d3d4a8678e200f4f043d93072257a5
Mehran RIzwan <165210723+mehrxn-cmd@users.noreply.github.com>
2025-06-15 14:39:22 +0530

    Event Model

--- ETS/Event.cs
+++ ETS/Event.cs
@@ -13,5 +13,35 @@ namespace ETS
         public DateTime Date { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
+        public int NoOfTickets { get; set; }
+        public string CreatedBy { get; set; }
+        public Event()
+        {
+        }
+
+        public Event(string name, DateTime date, string location, int noOfTickets, string createdBy)
+        {
+            Name = name;
+            Date = date;
+            Location = location;
+            NoOfTickets = noOfTickets;
+            CreatedBy = createdBy;
+        }
+
+        public Event(int eventID, string name, DateTime date, string location, int noOfTickets, string createdBy)
+        {
+            EventID = eventID;
+            Name = name;
+            Date = date;
+            Location = location;
+            NoOfTickets = noOfTickets;
+            CreatedBy = createdBy;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {EventID}, Name: {Name}, Date: {Date:yyyy-MM-dd}, Location: {Location}, Tickets: {NoOfTickets}, Created By: {CreatedBy}";
+        }
     }
 }
+
