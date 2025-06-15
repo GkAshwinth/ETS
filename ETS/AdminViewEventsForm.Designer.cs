1f02fc67da0fd8d8a4f84a76b19430a6030b55b3
affaqn1 <affaqahmed8411@gmail.com>
2025-06-15 13:19:55 +0530

    Admin View Events

--- ETS/AdminViewEventsForm.Designer.cs
+++ ETS/AdminViewEventsForm.Designer.cs
@@ -2,38 +2,51 @@
 {
     partial class AdminViewEventsForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridViewEvents;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewEvents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEvents
+            // 
+            this.dataGridViewEvents.AllowUserToAddRows = false;
+            this.dataGridViewEvents.AllowUserToDeleteRows = false;
+            this.dataGridViewEvents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEvents.Location = new System.Drawing.Point(20, 20);
+            this.dataGridViewEvents.Name = "dataGridViewEvents";
+            this.dataGridViewEvents.ReadOnly = true;
+            this.dataGridViewEvents.RowHeadersVisible = false;
+            this.dataGridViewEvents.RowTemplate.Height = 25;
+            this.dataGridViewEvents.Size = new System.Drawing.Size(740, 400);
+            this.dataGridViewEvents.TabIndex = 0;
+            // 
+            // AdminViewEventsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminViewEventsForm";
+            this.ClientSize = new System.Drawing.Size(780, 450);
+            this.Controls.Add(this.dataGridViewEvents);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AdminViewEventsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "All Events";
+            this.Load += new System.EventHandler(this.AdminViewEventsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).EndInit();
+            this.ResumeLayout(false);
         }
-
-        #endregion
     }
 }
--- ETS/AdminViewEventsForm.cs
+++ ETS/AdminViewEventsForm.cs
@@ -6,11 +6,25 @@ namespace ETS
     public partial class AdminViewEventsForm : Form
     {
         private User loggedInUser;
+        private AdminController adminController;
 
         public AdminViewEventsForm(User user)
         {
             InitializeComponent();
             loggedInUser = user;
+            adminController = new AdminController();
+        }
+
+        private void AdminViewEventsForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridViewEvents.DataSource = adminController.GetAllEvents();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load events: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
