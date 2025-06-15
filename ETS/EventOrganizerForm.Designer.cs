722046e01759a10a2a84d246c95270fe8d088240
Mehran RIzwan <165210723+mehrxn-cmd@users.noreply.github.com>
2025-06-15 14:48:38 +0530

    Events UI and Forms

---
+++ ETS/EventOrganizerForm.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace ETS
+{
+    partial class EventOrganizerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lblTitle = new Label();
+            lblName = new Label();
+            lblDate = new Label();
+            lblLocation = new Label();
+            lblTickets = new Label();
+            lblCreatedBy = new Label();
+            txtName = new TextBox();
+            txtLocation = new TextBox();
+            txtTickets = new TextBox();
+            txtCreatedBy = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            btnCreate = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            lblEventList = new Label();
+            btnRefresh = new Button();
+            dgvEvents = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvEvents).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(296, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(208, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Event Organizer Panel";
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.BackColor = SystemColors.ActiveCaption;
+            lblTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(76, 77);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(110, 21);
+            lblTitle.TabIndex = 1;
+            lblTitle.Text = "Event Details";
+            lblTitle.Click += label2_Click;
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.BackColor = SystemColors.ActiveCaption;
+            lblName.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblName.Location = new Point(40, 136);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(78, 15);
+            lblName.TabIndex = 2;
+            lblName.Text = "Event Name:";
+            // 
+            // lblDate
+            // 
+            lblDate.AutoSize = true;
+            lblDate.BackColor = SystemColors.ActiveCaption;
+            lblDate.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDate.Location = new Point(40, 170);
+            lblDate.Name = "lblDate";
+            lblDate.Size = new Size(72, 15);
+            lblDate.TabIndex = 3;
+            lblDate.Text = "Event Date:";
+            // 
+            // lblLocation
+            // 
+            lblLocation.AutoSize = true;
+            lblLocation.BackColor = SystemColors.ActiveCaption;
+            lblLocation.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblLocation.Location = new Point(40, 204);
+            lblLocation.Name = "lblLocation";
+            lblLocation.Size = new Size(57, 15);
+            lblLocation.TabIndex = 4;
+            lblLocation.Text = "Location:";
+            lblLocation.Click += label4_Click;
+            // 
+            // lblTickets
+            // 
+            lblTickets.AutoSize = true;
+            lblTickets.BackColor = SystemColors.ActiveCaption;
+            lblTickets.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTickets.Location = new Point(40, 239);
+            lblTickets.Name = "lblTickets";
+            lblTickets.Size = new Size(87, 15);
+            lblTickets.TabIndex = 5;
+            lblTickets.Text = "No. of Tickets:";
+            // 
+            // lblCreatedBy
+            // 
+            lblCreatedBy.AutoSize = true;
+            lblCreatedBy.BackColor = SystemColors.ActiveCaption;
+            lblCreatedBy.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCreatedBy.ForeColor = SystemColors.ControlText;
+            lblCreatedBy.Location = new Point(40, 275);
+            lblCreatedBy.Name = "lblCreatedBy";
+            lblCreatedBy.Size = new Size(71, 15);
+            lblCreatedBy.TabIndex = 6;
+            lblCreatedBy.Text = "Created By:";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(166, 133);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(200, 23);
+            txtName.TabIndex = 7;
+            txtName.TextChanged += txtName_TextChanged;
+            // 
+            // txtLocation
+            // 
+            txtLocation.Location = new Point(166, 201);
+            txtLocation.Name = "txtLocation";
+            txtLocation.Size = new Size(200, 23);
+            txtLocation.TabIndex = 9;
+            // 
+            // txtTickets
+            // 
+            txtTickets.Location = new Point(166, 236);
+            txtTickets.Name = "txtTickets";
+            txtTickets.Size = new Size(200, 23);
+            txtTickets.TabIndex = 10;
+            // 
+            // txtCreatedBy
+            // 
+            txtCreatedBy.Location = new Point(166, 272);
+            txtCreatedBy.Name = "txtCreatedBy";
+            txtCreatedBy.Size = new Size(200, 23);
+            txtCreatedBy.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(166, 164);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 12;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // btnCreate
+            // 
+            btnCreate.BackColor = Color.Green;
+            btnCreate.ForeColor = Color.White;
+            btnCreate.Location = new Point(44, 333);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(75, 30);
+            btnCreate.TabIndex = 13;
+            btnCreate.Text = "Create";
+            btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = Color.Orange;
+            btnUpdate.ForeColor = Color.White;
+            btnUpdate.Location = new Point(144, 333);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 30);
+            btnUpdate.TabIndex = 14;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = false;
+            btnUpdate.Click += button2_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.Red;
+            btnDelete.ForeColor = Color.White;
+            btnDelete.Location = new Point(248, 333);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 30);
+            btnDelete.TabIndex = 15;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = Color.Gray;
+            btnClear.ForeColor = Color.White;
+            btnClear.Location = new Point(347, 333);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 30);
+            btnClear.TabIndex = 16;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            // 
+            // lblEventList
+            // 
+            lblEventList.AutoSize = true;
+            lblEventList.BackColor = SystemColors.ActiveCaption;
+            lblEventList.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblEventList.Location = new Point(474, 77);
+            lblEventList.Name = "lblEventList";
+            lblEventList.Size = new Size(90, 21);
+            lblEventList.TabIndex = 17;
+            lblEventList.Text = "Events List";
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.BackColor = Color.Blue;
+            btnRefresh.ForeColor = Color.White;
+            btnRefresh.Location = new Point(757, 77);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(75, 25);
+            btnRefresh.TabIndex = 18;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = false;
+            btnRefresh.Click += btnRefresh_Click;
+            // 
+            // dgvEvents
+            // 
+            dgvEvents.AllowUserToAddRows = false;
+            dgvEvents.AllowUserToDeleteRows = false;
+            dgvEvents.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvEvents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEvents.Location = new Point(474, 133);
+            dgvEvents.MultiSelect = false;
+            dgvEvents.Name = "dgvEvents";
+            dgvEvents.ReadOnly = true;
+            dgvEvents.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvEvents.Size = new Size(382, 385);
+            dgvEvents.TabIndex = 19;
+            // 
+            // EventOrganizerForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 561);
+            Controls.Add(dgvEvents);
+            Controls.Add(btnRefresh);
+            Controls.Add(lblEventList);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnCreate);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(txtCreatedBy);
+            Controls.Add(txtTickets);
+            Controls.Add(txtLocation);
+            Controls.Add(txtName);
+            Controls.Add(lblCreatedBy);
+            Controls.Add(lblTickets);
+            Controls.Add(lblLocation);
+            Controls.Add(lblDate);
+            Controls.Add(lblName);
+            Controls.Add(lblTitle);
+            Controls.Add(label1);
+            Name = "EventOrganizerForm";
+            Text = "Event Organizer - ETS";
+            Load += EventOrganizerForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvEvents).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #region Control Declarations
+        private Label label1;
+        private Label lblTitle;
+        private Label lblName;
+        private Label lblDate;
+        private Label lblLocation;
+        private Label lblTickets;
+        private Label lblCreatedBy;
+        private TextBox txtName;
+        private TextBox txtLocation;
+        private TextBox txtTickets;
+        private TextBox txtCreatedBy;
+        private DateTimePicker dateTimePicker1;
+        private Button btnCreate;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnClear;
+        private Label lblEventList;
+        private Button btnRefresh; // THIS WAS MISSING!
+        private DataGridView dgvEvents;
+        #endregion
+
+    }
+}
+#endregion
---
+++ ETS/EventOrganizerForm.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ETS
+{
+    public partial class EventOrganizerForm : Form
+    {
+        private System.Windows.Forms.Timer autoEventTimer;
+        private List<EventTemplate> eventTemplates;
+        private EventController eventController;
+        private Event currentEvent;
+
+        public EventOrganizerForm()
+        {
+            InitializeComponent(); // This uses the designer-generated controls
+            eventController = new EventController();
+            currentEvent = null;
+            eventTemplates = new List<EventTemplate>(); // Add this
+            SetupEventHandlers(); // Setup event handlers for the designer controls
+            LoadEvents();
+            StartAutoEventGeneration();
+        }
+
+        // Setup event handlers for the controls created by the designer
+        private void SetupEventHandlers()
+        {
+            btnCreate.Click += BtnCreate_Click;
+            btnUpdate.Click += BtnUpdate_Click;
+            btnDelete.Click += BtnDelete_Click;
+            btnClear.Click += BtnClear_Click;
+            dgvEvents.CellClick += DgvEvents_CellClick;
+
+            // Set initial button states
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        private void LoadEvents()
+        {
+            try
+            {
+                List<Event> events = eventController.ReadEvents();
+
+                dgvEvents.DataSource = events.Select(e => new
+                {
+                    ID = e.EventID,
+                    Name = e.Name,
+                    Date = e.Date.ToString("yyyy-MM-dd HH:mm"),
+                    Location = e.Location,
+                    Tickets = e.NoOfTickets,
+                    CreatedBy = e.CreatedBy
+                }).ToList();
+
+                if (dgvEvents.Columns.Count > 0)
+                {
+                    dgvEvents.Columns["ID"].Width = 60;
+                    dgvEvents.Columns["Name"].Width = 150;
+                    dgvEvents.Columns["Date"].Width = 120;
+                    dgvEvents.Columns["Location"].Width = 120;
+                    dgvEvents.Columns["Tickets"].Width = 80;
+                    dgvEvents.Columns["CreatedBy"].Width = 100;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"❌ Error loading events: {ex.Message}", "Error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnCreate_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                Event newEvent = new Event
+                {
+                    Name = txtName.Text.Trim(),
+                    Date = dateTimePicker1.Value, // Use the designer control name
+                    Location = txtLocation.Text.Trim(),
+                    NoOfTickets = int.Parse(txtTickets.Text), // Convert from TextBox
+                    CreatedBy = txtCreatedBy.Text.Trim()
+                };
+
+                if (eventController.CreateEvent(newEvent))
+                {
+                    ClearForm();
+                    LoadEvents();
+                    MessageBox.Show("Event created successfully!", "Success",
+                                  MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (currentEvent != null && ValidateInput())
+            {
+                currentEvent.Name = txtName.Text.Trim();
+                currentEvent.Date = dateTimePicker1.Value;
+                currentEvent.Location = txtLocation.Text.Trim();
+                currentEvent.NoOfTickets = int.Parse(txtTickets.Text);
+
+                if (eventController.UpdateEvent(currentEvent))
+                {
+                    ClearForm();
+                    LoadEvents();
+                    MessageBox.Show("Event updated successfully!", "Success",
+                                  MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (currentEvent != null)
+            {
+                DialogResult result = MessageBox.Show(
+                    $"Are you sure you want to delete the event '{currentEvent.Name}'?",
+                    "Confirm Delete",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    if (eventController.DeleteEvent(currentEvent.EventID))
+                    {
+                        ClearForm();
+                        LoadEvents();
+                        MessageBox.Show("Event deleted successfully!", "Success",
+                                      MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void DgvEvents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvEvents.Rows[e.RowIndex];
+                int eventID = Convert.ToInt32(row.Cells["ID"].Value);
+
+                currentEvent = eventController.ReadEvent(eventID);
+
+                if (currentEvent != null)
+                {
+                    txtName.Text = currentEvent.Name;
+                    dateTimePicker1.Value = currentEvent.Date;
+                    txtLocation.Text = currentEvent.Location;
+                    txtTickets.Text = currentEvent.NoOfTickets.ToString();
+                    txtCreatedBy.Text = currentEvent.CreatedBy;
+
+                    btnUpdate.Enabled = true;
+                    btnDelete.Enabled = true;
+                    btnCreate.Enabled = false;
+                }
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Please enter event name.", "Validation Error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLocation.Text))
+            {
+                MessageBox.Show("Please enter event location.", "Validation Error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtLocation.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCreatedBy.Text))
+            {
+                MessageBox.Show("Please enter creator name.", "Validation Error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtCreatedBy.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtTickets.Text) || !int.TryParse(txtTickets.Text, out int tickets) || tickets <= 0)
+            {
+                MessageBox.Show("Please enter a valid number of tickets.", "Validation Error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTickets.Focus();
+                return false;
+            }
+
+            if (dateTimePicker1.Value <= DateTime.Now)
+            {
+                MessageBox.Show("Event date must be in the future.", "Validation Error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                dateTimePicker1.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void StartAutoEventGeneration()
+        {
+            // Check every 30 minutes for events to create
+            autoEventTimer = new System.Windows.Forms.Timer();
+            autoEventTimer.Interval = 30 * 60 * 1000; // 30 minutes
+            autoEventTimer.Tick += AutoEventTimer_Tick;
+            autoEventTimer.Start();
+
+            // Create initial template
+            CreateDefaultTemplate();
+        }
+
+        private void AutoEventTimer_Tick(object sender, EventArgs e)
+        {
+            GenerateAutomaticEvents();
+        }
+
+        private void CreateDefaultTemplate()
+        {
+            // Add a default template for automatic event generation
+            eventTemplates.Add(new EventTemplate
+            {
+                Name = "Weekly Concert",
+                Location = "Main Auditorium",
+                NoOfTickets = 100,
+                CreatedBy = "System",
+                IntervalDays = 7, // Every 7 days
+                StartDate = DateTime.Now.AddDays(1),
+                EventTime = new TimeSpan(19, 0, 0) // 7:00 PM
+            });
+        }
+
+        private void GenerateAutomaticEvents()
+        {
+            try
+            {
+                DateTime futureDate = DateTime.Now.AddDays(7); // Plan events 1 week ahead
+
+                foreach (var template in eventTemplates)
+                {
+                    // Check if we need to create an event for this template
+                    if (ShouldCreateEvent(template, futureDate))
+                    {
+                        Event autoEvent = new Event
+                        {
+                            Name = $"{template.Name} - {futureDate:MMM dd}",
+                            Date = futureDate.Date.Add(template.EventTime),
+                            Location = template.Location,
+                            NoOfTickets = template.NoOfTickets,
+                            CreatedBy = template.CreatedBy + " (Auto)"
+                        };
+
+                        if (eventController.CreateEvent(autoEvent))
+                        {
+                            LoadEvents(); // Refresh the display
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Auto-generation error: {ex.Message}", "Error");
+            }
+        }
+
+        private bool ShouldCreateEvent(EventTemplate template, DateTime targetDate)
+        {
+            // Check if event already exists for this date and template
+            List<Event> existingEvents = eventController.ReadEvents();
+            string expectedName = $"{template.Name} - {targetDate:MMM dd}";
+
+            return !existingEvents.Any(e =>
+                e.Name.Contains(template.Name) &&
+                e.Date.Date == targetDate.Date);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+                autoEventTimer?.Stop(); // Add this
+                autoEventTimer?.Dispose(); // Add this
+            }
+            base.Dispose(disposing);
+        }
+
+        private void ClearForm()
+        {
+            txtName.Clear();
+            dateTimePicker1.Value = DateTime.Now.AddDays(1);
+            txtLocation.Clear();
+            txtTickets.Clear();
+            txtCreatedBy.Clear();
+
+            currentEvent = null;
+
+            btnCreate.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        // Event handlers for designer-generated events (keep these empty if not needed)
+        private void EventOrganizerForm_Load(object sender, EventArgs e)
+        {
+            // Form load logic if needed
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Label click handler if needed
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            // Label click handler if needed
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // This is linked to btnUpdate in the designer
+            // The actual logic is now in BtnUpdate_Click
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            // Date picker change handler if needed
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
