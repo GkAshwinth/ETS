e3e2c9dc2fd11d4e44fabdec339acf21183098eb
Mehran RIzwan <165210723+mehrxn-cmd@users.noreply.github.com>
2025-06-15 14:42:01 +0530

    Event Template Model

---
+++ ETS/EventTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS
+{
+    public class EventTemplate
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int NoOfTickets { get; set; }
+        public string CreatedBy { get; set; }
+        public int IntervalDays { get; set; }
+        public DateTime StartDate { get; set; }
+        public TimeSpan EventTime { get; set; }
+    }
+}
